03a81ff76495e47a0aaccf51d79d7d90f6ee99b5
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-16 20:20:41 +0530

    code updated

--- attachments/02-nextjs-essentials/07-adding-a-custom-component-to-layout/app/page.tsx
+++ attachments/02-nextjs-essentials/07-adding-a-custom-component-to-layout/app/page.tsx
@@ -6,9 +6,15 @@ export default function Home() {
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
-      <p><Link href="/meals">Meals</Link></p>
-      <p><Link href="/meals/share">Share Meal</Link></p>
-      <p><Link href="/community">Community</Link></p>
+      <p>
+        <Link href='/meals'>Meals</Link>
+      </p>
+      <p>
+        <Link href='/meals/share'>Share Meal</Link>
+      </p>
+      <p>
+        <Link href='/community'>Community</Link>
+      </p>
     </main>
   );
 }
