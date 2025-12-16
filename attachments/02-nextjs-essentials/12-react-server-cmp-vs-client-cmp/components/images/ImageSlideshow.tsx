88cbc55ff99a8073dd5302158c13fba801ae2780
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-16 20:58:00 +0530

    code updated

--- attachments/02-nextjs-essentials/12-react-server-cmp-vs-client-cmp/components/images/ImageSlideshow.tsx
+++ attachments/02-nextjs-essentials/12-react-server-cmp-vs-client-cmp/components/images/ImageSlideshow.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
-
 import burgerImg from '@/assets/burger.jpg';
 import curryImg from '@/assets/curry.jpg';
 import dumplingsImg from '@/assets/dumplings.jpg';
@@ -10,7 +9,7 @@ import macncheeseImg from '@/assets/macncheese.jpg';
 import pizzaImg from '@/assets/pizza.jpg';
 import schnitzelImg from '@/assets/schnitzel.jpg';
 import tomatoSaladImg from '@/assets/tomato-salad.jpg';
-import classes from './image-slideshow.module.scss';
+import classes from './ImageSlideshow.module.scss';
 
 const images = [
   { image: burgerImg, alt: 'A delicious, juicy burger' },
@@ -22,7 +21,7 @@ const images = [
   { image: tomatoSaladImg, alt: 'A delicious tomato salad' },
 ];
 
-export default function ImageSlideshow() {
+export const ImageSlideshow = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
@@ -47,4 +46,4 @@ export default function ImageSlideshow() {
       ))}
     </div>
   );
-}
+};
