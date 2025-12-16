0d15dc7d43c24b2f9652ab72f206966032fa380c
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-16 23:11:49 +0530

    code updated

--- attachments/02-nextjs-essentials/17-using-suspense/app/community/page.tsx
+++ attachments/02-nextjs-essentials/17-using-suspense/app/community/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-
 import mealIcon from '@/assets/icons/meal.png';
 import communityIcon from '@/assets/icons/community.png';
 import eventsIcon from '@/assets/icons/events.png';
@@ -19,17 +18,17 @@ export default function CommunityPage() {
 
         <ul className={classes.perks}>
           <li>
-            <Image src={mealIcon} alt="A delicious meal" />
+            <Image src={mealIcon} alt='A delicious meal' />
             <p>Share & discover recipes</p>
           </li>
           <li>
-            <Image src={communityIcon} alt="A crowd of people, cooking" />
+            <Image src={communityIcon} alt='A crowd of people, cooking' />
             <p>Find new friends & like-minded people</p>
           </li>
           <li>
             <Image
               src={eventsIcon}
-              alt="A crowd of people at a cooking event"
+              alt='A crowd of people at a cooking event'
             />
             <p>Participate in exclusive events</p>
           </li>
