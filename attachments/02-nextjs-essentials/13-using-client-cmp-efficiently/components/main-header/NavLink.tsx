1df9ac8b86539e3bba15b6cd6379869ee6944fd0
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-16 22:49:20 +0530

    code updated

--- attachments/02-nextjs-essentials/13-using-client-cmp-efficiently/components/main-header/NavLink.tsx
+++ attachments/02-nextjs-essentials/13-using-client-cmp-efficiently/components/main-header/NavLink.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import { ReactNode } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import classes from './nav-link.module.scss';
+import { NavLinkProps } from 'models';
+import classes from './NavLink.module.scss';
 
-export const NavLink = ({
-  href,
-  children,
-}: {
-  href: string;
-  children: ReactNode;
-}) => {
+export const NavLink = ({ href, children }: NavLinkProps) => {
   const path = usePathname();
 
   return (
---
+++ attachments/02-nextjs-essentials/13-using-client-cmp-efficiently/models/index.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react';
+
+export interface NavLinkProps {
+  href: string;
+  children: ReactNode;
+}
