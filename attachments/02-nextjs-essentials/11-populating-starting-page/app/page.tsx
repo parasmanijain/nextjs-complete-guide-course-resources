7ed37716f815579a7e7d172ca30153b6c181f536
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-16 20:49:01 +0530

    code updated

--- attachments/02-nextjs-essentials/11-populating-starting-page/app/page.tsx
+++ attachments/02-nextjs-essentials/11-populating-starting-page/app/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-
 import classes from './page.module.scss';
 
 export default function Home() {
@@ -13,8 +12,8 @@ export default function Home() {
             <p>Taste & share food from all over the world.</p>
           </div>
           <div className={classes.cta}>
-            <Link href="/community">Join the Community</Link>
-            <Link href="/meals">Explore Meals</Link>
+            <Link href='/community'>Join the Community</Link>
+            <Link href='/meals'>Explore Meals</Link>
           </div>
         </div>
       </header>
@@ -23,8 +22,8 @@ export default function Home() {
           <h2>How it works</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect
@@ -36,8 +35,8 @@ export default function Home() {
           <h2>Why NextLevel Food?</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect
