b1627ff537e54e093dfc582230d263d1ccd2dbfb
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-31 17:44:39 +0530

    code updated

--- attachments/02-nextjs-essentials/25-storing-server-actions-in-separate-file/lib/actions.ts
+++ attachments/02-nextjs-essentials/25-storing-server-actions-in-separate-file/lib/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-export async function shareMeal(formData) {
+export async function shareMeal(formData: FormData) {
   const meal = {
     title: formData.get('title'),
     summary: formData.get('summary'),
